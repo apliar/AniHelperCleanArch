26ab81668f043f6814ca534adf5216fc2afde415
apliar <75115989+apliar@users.noreply.github.com>
2022-04-09 16:12:23 +0500

    Add ui part

---
+++ AniHelper.API/Controllers/AnimeController.cs
@@ -0,0 +1,34 @@
+﻿using AniHelper.Application.Commands;
+using AniHelper.Application.Queries;
+using AniHelper.Application.Responses;
+using AniHelper.Core.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniHelper.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AnimeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IReadOnlyList<Anime>> Get()
+        {
+            return await _mediator.Send(new GetAllAnimeQuery());
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<AnimeResponse>> CreateAnime([FromBody] CreateAnimeCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
