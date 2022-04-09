7786bcfc1478796258c9e1365d9df13f5e7590cd
apliar <75115989+apliar@users.noreply.github.com>
2022-04-09 16:15:08 +0500

    Add infrastructure

---
+++ AniHelper.Infrastructure/Data/AnimeContext.cs
@@ -0,0 +1,16 @@
+﻿using AniHelper.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Infrastructure.Data
+{
+    public class AnimeContext: DbContext
+    {
+        public AnimeContext(DbContextOptions<AnimeContext> options) : base(options) { }
+        public DbSet<Anime> Animes { get; set; }
+    }
+}
