5af664b2857455272dfb078f910583b373389ebc
apliar <75115989+apliar@users.noreply.github.com>
2022-04-09 18:19:26 +0500

    Add files via upload

---
+++ AniHelper.Application/Queries/GetAllAnimeQuery.cs
@@ -0,0 +1,20 @@
+﻿using AniHelper.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Application.Queries
+{
+    public class GetAllAnimeQuery : IRequest<IReadOnlyList<Anime>>
+    {
+        public int AnimeId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Synopsis { get; set; }
+        public GetAllAnimeQuery() { }
+    }
+}
---
+++ AniHelper.Application/Responses/AnimeResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Application.Responses
+{
+    public class AnimeResponse
+    {
+        public int AnimeId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Synopsis { get; set; }
+    }
+}
