a3ba6591d911abce6b04c2b2e23c1782aeab400e
apliar <75115989+apliar@users.noreply.github.com>
2022-04-09 16:14:28 +0500

    Add domain pt2

---
+++ AniHelper.Core/Entities/Anime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Core.Entities
+{
+    public class Anime
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 AnimeId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Synopsis { get; set; }
+    }
+}
---
+++ AniHelper.Core/Repositories/IAnimeRepository.cs
@@ -0,0 +1,15 @@
+﻿using AniHelper.Core.Entities;
+using AniHelper.Core.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Core.Repositories
+{
+    public interface IAnimeRepository: IRepository<Anime>
+    {
+        Task<IEnumerable<Anime>> GetAnimeByName(string name);
+    }
+}
