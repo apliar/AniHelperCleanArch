25637224055968f122a90cc2cad156d91c37e425
apliar <75115989+apliar@users.noreply.github.com>
2022-04-09 18:20:28 +0500

    Add files via upload

---
+++ AniHelper.Infrastructure/Repositories/AnimeRepository.cs
@@ -0,0 +1,22 @@
+﻿using AniHelper.Core.Entities;
+using AniHelper.Core.Repositories;
+using AniHelper.Infrastructure.Data;
+using AniHelper.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Infrastructure.Repositories
+{
+    public class AnimeRepository: Repository<Anime>, IAnimeRepository
+    {
+        public AnimeRepository(AnimeContext animeContext) : base(animeContext) { }
+        public async Task<IEnumerable<Anime>> GetAnimeByName(string name)
+        {
+            return await _animeContext.Animes.Where(m => m.Name == name).ToListAsync();
+        }
+    }
+}
---
+++ AniHelper.Infrastructure/Repositories/Base/Repository.cs
@@ -0,0 +1,43 @@
+﻿using AniHelper.Core.Repositories.Base;
+using AniHelper.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniHelper.Infrastructure.Repositories.Base
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AnimeContext _animeContext;
+        public Repository(AnimeContext animeContext)
+        {
+            _animeContext = animeContext;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _animeContext.Set<T>().AddAsync(entity);
+            await _animeContext.SaveChangesAsync();
+            return entity;
+        }
+        public async Task DeleteAsync(T entity)
+        {
+            _animeContext.Set<T>().Remove(entity);
+            await _animeContext.SaveChangesAsync();
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _animeContext.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _animeContext.Set<T>().FindAsync(id);
+        }
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
